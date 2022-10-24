9f6bfdcf2d41d2ef526e0db8a00ebaa5ba81b297
yoffrey <76462775+yoffrey@users.noreply.github.com>
2022-10-23 23:03:39 -0700

    fixed merge conflicts
    - fix merge conflict
    - reduce number of api calls

--- recorder.py
+++ recorder.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime, timezone, timedelta # https://docs.python.org/3/library/datetime.html
+from datetime import date, datetime, timezone # https://docs.python.org/3/library/datetime.html
 import ffmpeg # https://kkroening.github.io/ffmpeg-python/
 import sched, time # https://pythontic.com/concurrency/scheduler/introduction
 import os
@@ -54,10 +54,10 @@ def getTodaysShows():
 		showStart = parser.parse(showInfo['start']).replace(tzinfo=timezone.utc).astimezone(tz=None)
 		showEnd = parser.parse(showInfo['end']).replace(tzinfo=timezone.utc).astimezone(tz=None)
 		duration = showInfo['duration']
-		hrefLink = showInfo["_links"]["personas"][0]["href"]
-		email = getEmail(hrefLink)
 		# Only add show if it starts the same day and is not autoplay
 		if showInfo['category'] != 'Automation' and showStart.date()==date.today():
+			hrefLink = showInfo["_links"]["personas"][0]["href"]
+			email = getEmail(hrefLink)
 			# Need to add API hit to get email address
 			todaysRecordingSchedule.append({
 					'showName': showName,
@@ -67,30 +67,40 @@ def getTodaysShows():
 			})
 	return todaysRecordingSchedule
 
-def sendToDJ(fileName, email):
-	# https://show-bucket-test.s3.us-west-1.amazonaws.com/2022-10-17_TheSaladBowl.mp3
-	downloadStr = 'https://show-bucket-test.s3.us-west-1.amazonaws.com/' + fileName
+def sendToDJ(fileName, email, showName):
+	# https://kscu.s3.us-west-1.amazonaws.com/2022-10-17_TheSaladBowl.mp3
+	downloadStr = 'https://kscu.s3.us-west-1.amazonaws.com/' + fileName
 
 	s = smtplib.SMTP('smtp.gmail.com', 587)
 	s.starttls()
 	s.login(EMAIL, PASSWORD)
-	SUBJECT = 'Recording Link'
-	TEXT = "Here is the recording to your show. \nLink is valid for 5 days.\n" + downloadStr
-	message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
-	s.sendmail(EMAIL, "jeffreychen2168@gmail.com", message)
+	SUBJECT = f"Recording Link - {showName}"
+	text = """
+	Howdy!
+	Hey!
+	
+	We're testing a new system to automatically record and send you your shows.
+	This link is valid for 5 days so please save this file to your local machine.
+
+	If there's any issues with this system, please let us know by emailing gm@kscu.org.
+	
+	Peace to every crease on your brain,
+	KSCU <3
+	"""
+	text += downloadStr
+	message = 'Subject: {}\n\n{}'.format(SUBJECT, text)
+	s.sendmail(EMAIL, email, message)
 	s.quit()
 
 def sendToS3(todaysRecordingSchedule):
 	# Old files can be removed automatically through S3
 	# Send files from EC2 to S3 and delete them in EC2
-	# show-bucket-test needs to be updated to final account
 	for showInfo in todaysRecordingSchedule:
 		fileName = str(showInfo['showStart'].date()) + '_' + showInfo['showName'] + '.mp3'
-		sendStr = 'aws s3 cp ' + fileName + ' s3://show-bucket-test'
-		#email = ...
+		sendStr = 'aws s3 cp ' + fileName + ' s3://kscu'
 		os.system(sendStr)
 		os.system('rm ' + fileName)
-		sendToDJ(fileName)
+		sendToDJ(fileName, showInfo["email"], showInfo["showName"])
 
 def record(duration, fileName):
 	# Create string in the format below:
@@ -118,5 +128,5 @@ while True:
 	currentTime = datetime.now().strftime("%H:%M")
 	if currentTime == "06:55":
 		runSchedule(getTodaysShows())
-	time.sleep(1)
+	time.sleep(30)
 
